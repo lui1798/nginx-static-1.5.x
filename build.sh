96ec9194f72461567904b284047e18228ecc4397
Michael Wyraz <michael.wyraz@evermind.de>
2016-03-11 07:41:33 +0100

    packing

--- build.sh
+++ build.sh
@@ -6,10 +6,13 @@ sudo apt-get install libxslt1-dev libxml2-dev zlib1g-dev libpcre3-dev libbz2-dev
 
 # download nginx and openssl
 
-wget http://nginx.org/download/nginx-1.5.13.tar.gz
-tar xf nginx-1.5.13.tar.gz; cd nginx-1.5.13
-wget http://www.openssl.org/source/openssl-1.0.1s.tar.gz
-tar xf openssl-1.0.1s.tar.gz
+NGINX_VERSION=1.5.13
+OPENSSL_VERSION=1.0.1s
+
+wget http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
+tar xf nginx-${NGINX_VERSION}.tar.gz; cd nginx-${NGINX_VERSION}
+wget http://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz
+tar xf openssl-${OPENSSL_VERSION}.tar.gz
 
 # configure - this configuration enables every possible module, except:
 #
@@ -34,7 +37,7 @@ tar xf openssl-1.0.1s.tar.gz
 #  1) logging to /opt/nginx/logs/error.log 
 #  2) reading cfg file from /opt/nginx/conf/nginx.conf
 
-./configure --prefix=../nginx-static --with-cc-opt="-static -static-libgcc" \
+./configure --prefix=nginx-static --with-cc-opt="-static -static-libgcc" \
             --with-ld-opt="-static" --with-cpu-opt=generic --with-pcre \
             --with-mail --with-ipv6 --with-poll_module --with-select_module \
             --with-rtsig_module --with-select_module --with-poll_module \
@@ -44,12 +47,17 @@ tar xf openssl-1.0.1s.tar.gz
             --with-http_gzip_static_module --with-http_auth_request_module \
             --with-http_random_index_module --with-http_secure_link_module \
             --with-http_degradation_module --with-http_stub_status_module \
-            --with-mail --with-mail_ssl_module --with-openssl=./openssl-1.0.1s
+            --with-mail --with-mail_ssl_module --with-openssl=./${OPENSSL_VERSION}
 
 # with -j > 1 nginx's tries to link openssl before it gets built
 
 make -j1
 make -j1 install
+
+echo "Nginx: ${NGINX_VERSION}" > nginx-static/versions.txt
+echo "OpenSSL: ${OPENSSL_VERSION}" >> nginx-static/versions.txt
+
+tar cfvz ../nginx-static.tgz nginx-static
 
 # nginx binary built on Debian Etch amd64 (gcc 4.1):
 # https://dl.dropboxusercontent.com/u/90980609/nginx-1.5.6-amd64.tar.bz2
